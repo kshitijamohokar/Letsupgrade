1752a828e3e7048b63f0a5f97b6144d6bdff1ac5
manikantatarun <50768140+manikantatarun@users.noreply.github.com>
2020-09-05 11:04:23 +0530

    Add files via upload

---
+++ A1-D3.py
@@ -0,0 +1,7 @@
+value = int(input("Enter the alltitude"))
+if value <= 1000:
+   print("Safe to Land") 
+elif value > 1000 and value<=5000:
+   print("Bring Down To 1000") 
+else:
+   print("Turn Around ")
