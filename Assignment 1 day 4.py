9269a2a2a676fafc467cb3ee28a94d3eae1ffd6d
manikantatarun <50768140+manikantatarun@users.noreply.github.com>
2020-09-05 11:36:43 +0530

    Add files via upload

---
+++ Assignment 1 day 4.py
@@ -0,0 +1,12 @@
+lower = 1042000
+upper = 702648265
+for num in range(lower,upper + 1):  
+   sum = 0  
+   temp = num  
+   while temp > 0:  
+       digit = temp % 10  
+       sum += digit ** 3  
+       temp //= 10  
+       if num == sum:  
+            print(num)
+   	
