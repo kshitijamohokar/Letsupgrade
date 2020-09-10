196741772a46bbc518e085710007ad4b25658cb7
manikantatarun <50768140+manikantatarun@users.noreply.github.com>
2020-09-10 19:12:09 +0530

    Update Day_3 Assignment .py

--- Day_3 Assignment .py
+++ Day_3 Assignment .py
@@ -1,3 +1,15 @@
+Question 1 >>>
+
+value = int(input("Enter the alltitude"))
+if value <= 1000:
+   print("Safe to Land") 
+elif value > 1000 and value<=5000:
+   print("Bring Down To 1000") 
+else:
+   print("Turn Around ")
+
+Question 2 >>>
+
 for num in range(1 , 201):
 	flag = True
 	for i in range(2 , (num//2+1)):
